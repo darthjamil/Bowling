10a3cd4945ecdef747aa388086d2a7a08d87658c
Nick Jamil <nickjamil@hotmail.com>
2020-04-08 17:17:09 -0400

    Implement Bowling with TDD

--- Bowling.App/Game.cs
+++ Bowling.App/Game.cs
@@ -1,9 +1,69 @@
-﻿using System;
-
+﻿
 namespace Bowling.App
 {
     public class Game
     {
+        private const int PERFECT_ROLL_SCORE = 10;
 
+        private readonly int[] Rolls = new int[21];
+        private int CurrentFrame = 0;
+
+        public void Roll(int pins)
+        {
+            Rolls[CurrentFrame] = pins;
+            CurrentFrame++;
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int currentRoll = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(currentRoll))
+                {
+                    score += GetScoreForStrike(currentRoll);
+                    currentRoll += 1;
+                }
+                else if (IsSpare(currentRoll))
+                {
+                    score += GetScoreForSpare(currentRoll);
+                    currentRoll += 2;
+                }
+                else
+                {
+                    score += GetScoreForRoll(currentRoll);
+                    currentRoll += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private int GetScoreForRoll(int i)
+        {
+            return Rolls[i] + Rolls[i + 1];
+        }
+
+        private int GetScoreForSpare(int i)
+        {
+            return PERFECT_ROLL_SCORE + Rolls[i + 2];
+        }
+
+        private int GetScoreForStrike(int i)
+        {
+            return PERFECT_ROLL_SCORE + Rolls[i + 1] + Rolls[i + 2];
+        }
+
+        private bool IsStrike(int i)
+        {
+            return Rolls[i] == PERFECT_ROLL_SCORE;
+        }
+
+        private bool IsSpare(int i)
+        {
+            return Rolls[i] + Rolls[i + 1] == PERFECT_ROLL_SCORE;
+        }
     }
 }
--- Bowling.UnitTests/TestGame.cs
+++ Bowling.UnitTests/TestGame.cs
@@ -5,15 +5,93 @@ namespace Bowling.UnitTests
 {
     public class TestGame
     {
+        private Game Game;
+
         [SetUp]
         public void Setup()
         {
+            Game = new Game();
         }
 
         [Test]
-        public void CanCreateGame()
+        public void GutterGame()
         {
-            new Game();
+            RollMany(20, 0);
+            Assert.That(Game.GetScore(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AllOnes()
+        {
+            RollMany(20, 1);
+            Assert.That(Game.GetScore(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void AllTwos()
+        {
+            RollMany(20, 2);
+            Assert.That(Game.GetScore(), Is.EqualTo(40));
+        }
+
+        [Test]
+        public void OneSpare()
+        {
+            RollSpare();
+            RollMany(18, 1);
+            Assert.That(Game.GetScore(), Is.EqualTo(29));
+        }
+
+        [Test]
+        public void OneStrike()
+        {
+            RollStrike();
+            RollMany(18, 1);
+            Assert.That(Game.GetScore(), Is.EqualTo(30));
+        }
+
+        [Test]
+        public void OneStrikeInMiddle()
+        {
+            RollMany(2, 1);
+            RollStrike();
+            RollMany(16, 1);
+            Assert.That(Game.GetScore(), Is.EqualTo(30));
+        }
+
+        [Test]
+        public void OneSpareInMiddle()
+        {
+            RollMany(2, 1);
+            RollSpare();
+            RollMany(16, 1);
+            Assert.That(Game.GetScore(), Is.EqualTo(29));
+        }
+
+        [Test]
+        public void PerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.That(Game.GetScore(), Is.EqualTo(300));
+        }
+
+        private void RollStrike()
+        {
+            Game.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            Game.Roll(5);
+            Game.Roll(5);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                Game.Roll(pins);
+            }
         }
     }
 }
